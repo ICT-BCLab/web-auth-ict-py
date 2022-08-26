48badb34c5d6205ac1b234ca9404a78169968c1c
longbo <15940992407@163.com>
2022-08-26 17:02:46 +0800

    update readme

--- web-auth-ict.py
+++ web-auth-ict.py
@@ -6,7 +6,7 @@ import time
 import os
 
 os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.realpath(__file__))
-driver = webdriver.PhantomJS("./phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
+driver = webdriver.PhantomJS("./tools/phantomjs-2.1.1-linux-x86_64/bin/phantomjs")
 driver.get('https://gw.ict.ac.cn/srun_portal_pc?ac_id=1&theme=pro')
 name = driver.find_element_by_id("username")
 name.send_keys('blockchain_test_1')
