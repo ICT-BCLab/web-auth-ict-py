e203c99d7b279e1f11787bdb7364973025566aa3
stormpang <jialinpeng@ict.ac.cn>
2022-08-26 15:37:21 +0800

    init

---
+++ web-auth-ict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
+
+os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.realpath(__file__))
+driver = webdriver.PhantomJS()
+driver.get('https://gw.ict.ac.cn/srun_portal_pc?ac_id=1&theme=pro')
+name = driver.find_element_by_id("username")
+name.send_keys('blockchain_test_1')
+password = driver.find_element_by_id('password')
+password.send_keys('ictblockchain706')
+password.send_keys(Keys.RETURN)
+login =  driver.find_element_by_id( "login-account")
+login.click()
+time.sleep(1)
+driver.quit()
